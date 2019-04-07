2200c5b218b612bc14da3e6642dda42cb32706fd
Hubert Siwkin <hubertsiwkin@Huberts-MacBook-Pro.local>
2019-04-07 11:31:56 +0200

    Add test checking res on GET on /
    Signed-off-by: Hubert Siwkin <hubertsiwkin@Huberts-MacBook-Pro.local>

--- test/base.router.test.js
+++ test/base.router.test.js
@@ -1,3 +1,17 @@
+const express = require('express');
+const supertest = require('supertest');
+
+const router = require('../web/routing/base.router');
+
 it ("tests if jasmine works", () => {
     expect(1).toBe(1);
+});
+
+it("returns proper response on GET on /", async () => {
+    const app = express();
+    router(app);
+
+    const res = await supertest(app).get('/').expect(200);
+
+    expect(res.body.status).toEqual('OK');
 });
